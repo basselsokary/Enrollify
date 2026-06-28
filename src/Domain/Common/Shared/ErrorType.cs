1be1bd453de77402bb8ca0a657ce17d3e2fb4272
basselsokary <basselelsokary@gmail.com>
2026-06-28 23:25:59 +0300

    Adding Shared folder that contains the Result and Error patterns

---
+++ src/Domain/Common/Shared/ErrorType.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Common.Shared;
+
+public enum ErrorType
+{
+    None,
+    Failure,
+    NotFound,
+    Validation,
+    Conflict,
+    Unauthorized,
+    Forbidden,
+    General
+}
