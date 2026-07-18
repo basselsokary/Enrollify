0f1da0baeec31926cbc200b1e0efdc22c9b7ca34
basselsokary <basselelsokary@gmail.com>
2026-07-18 21:45:08 +0300

    Refactor IUserContext interface by removing role-related methods and unused using directive

--- src/Application/Common/Interfaces/Authentication/IUserContext.cs
+++ src/Application/Common/Interfaces/Authentication/IUserContext.cs
@@ -1,13 +1,8 @@
-﻿using Application.Contracts.Users;
-
-namespace Application.Common.Interfaces.Authentication;
+﻿namespace Application.Common.Interfaces.Authentication;
 
 public interface IUserContext
 {
     Guid Id { get; }
     string UserName { get; }
-    bool IsInRoles(params string[] roles);
-    bool IsInAnyRole(params string[] roles);
     bool IsAuthenticated { get; }
-    UserDto User { get; }
 }
