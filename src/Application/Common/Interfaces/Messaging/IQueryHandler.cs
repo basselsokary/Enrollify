7a2098e0450eee8fedef09465513bf8a75643c73
basselsokary <basselelsokary@gmail.com>
2026-07-18 21:42:35 +0300

    abstracting for commands and queries

---
+++ src/Application/Common/Interfaces/Messaging/IQueryHandler.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Common.Shared;
+
+namespace Application.Common.Interfaces.Messaging;
+
+public interface IQueryHandler<in TQuery, TResponse>
+    where TQuery : IQuery<TResponse>
+{
+    Task<Result<TResponse>> HandleAsync(TQuery query, CancellationToken cancellationToken = default);
+}
+    
